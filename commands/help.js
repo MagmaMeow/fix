584b184f708434a0bee77d1b0620032198b95b27
MagmaMeow <Kannunanu024@gmail.com>
2025-09-17 18:18:38 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  name: "ping", // for message commands
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interactionOrMessage) {
+    if (interactionOrMessage.reply) {
+      await interactionOrMessage.reply("🏓 Pong!");
+    }
+  }
+};
+
 module.exports = {
   name: "help",
   description: "List all commands",
@@ -13,3 +27,4 @@ module.exports = {
     interaction.reply("📖 Commands:\n" + cmds);
   }
 };
+
