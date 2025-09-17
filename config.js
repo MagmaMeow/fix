c97a8aa447710b4964715c0e8df2af1cfe47a566
MagmaMeow <Kannunanu024@gmail.com>
2025-09-17 15:53:28 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  prefixes: ["!", "?"], // multiple prefixes
+  ownerId: "1268229530351567032", // your ID
+  hubGuildId: "1417807760791179346", // hub guild
+  logChannelId: process.env.LOG_CHANNEL_ID // set in Render env
+};
